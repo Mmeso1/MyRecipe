5c83a70b0477a8d9074dbbc4e1336ba8d398831d
Mmeso1 <anisiubammesoma8@gmail.com>
2024-12-21 10:24:57 +0000

    I configured the app route to use \/api instead of \/admin

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Recipe
-from serializers import RecipeSerializer
+from .serializers import RecipeSerializer
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
